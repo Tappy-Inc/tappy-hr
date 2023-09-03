3f534203c3d2ad27a9ddb2c3608f651adc343572
Gelo <chrisgelosulit@gmail.com>
2023-09-03 13:50:26 +0800

    Added Elastic UI

--- apps/manpower-request-app/pages/_app.tsx
+++ apps/manpower-request-app/pages/_app.tsx
@@ -1,17 +1,21 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { EuiProvider } from '@elastic/eui';
+
 import './styles.css';
+import '@elastic/eui/dist/eui_theme_light.css';
+
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+     <EuiProvider colorMode="light">
       <Head>
         <title>Welcome to manpower-request-app!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </EuiProvider>
   );
 }
 
